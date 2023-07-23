0d73ef7cacb1207629c343b33e8e55fd72ce692a
Bartosz Michalski <b.michalski@ideo.pl>
2023-07-23 16:24:38 +0200

    [Invoice] Validation for nip added

--- IntuitivePaper.Application/Invoice/Commands/CreateInvoice/CreateInvoiceCommandValidator.cs
+++ IntuitivePaper.Application/Invoice/Commands/CreateInvoice/CreateInvoiceCommandValidator.cs
@@ -15,10 +15,14 @@ namespace IntuitivePaper.Application.Invoice.Commands.CreateInvoice
             RuleFor(x => x.Date).NotEmpty().WithMessage("Data wystawienia jest wymagana.");
             RuleFor(x => x.SellerName).NotEmpty().WithMessage("Nazwa sprzedawcy jest wymagana.");
             RuleFor(x => x.SellerAddress).NotEmpty().WithMessage("Adres sprzedawcy jest wymagany.");
-            RuleFor(x => x.SellerTaxId).NotEmpty().MaximumLength(50).WithMessage("NIP sprzedawcy jest wymagany.");
+            RuleFor(x => x.SellerTaxId)
+                .NotEmpty().WithMessage("NIP sprzedawcy jest wymagany.")
+                .Matches(@"^\d{10}$").WithMessage("NIP sprzedawcy musi zawierać 10 cyfr.");
             RuleFor(x => x.BuyerName).NotEmpty().WithMessage("Nazwa nabywcy jest wymagana.");
             RuleFor(x => x.BuyerAddress).NotEmpty().WithMessage("Adres nabywcy jest wymagany.");
-            RuleFor(x => x.BuyerTaxId).NotEmpty().MaximumLength(50).WithMessage("NIP nabywcy jest wymagany.");
+            RuleFor(x => x.BuyerTaxId)
+                .NotEmpty().WithMessage("NIP nabywcy jest wymagany.")
+                .Matches(@"^\d{10}$").WithMessage("NIP nabywcy musi zawierać 10 cyfr.");
         }
     }
 
